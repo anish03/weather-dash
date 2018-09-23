989f8fd29261bc528cc135722d68be0df5a58a67
anish03 <anish.narkhede@gmail.com>
2018-09-22 21:53:38 -0700

    registration and login implemented

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '2916e6522e3f4b5282af6896183a5fcb'
 
 cities = [
     {'name':'San Jose'},
@@ -8,12 +12,33 @@ cities = [
 
 @app.route("/")
 @app.route("/home ")
-def hello():
+def home():
     return render_template('index.html',cities=cities)
 
 @app.route("/about")
 def about():
     return 'About page'
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {}!'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
